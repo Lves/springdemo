9ae1b7c5e0f1fd249a3d75a3aa214ccc84dffed4
lveslxl@gmail.com <lveslxl@gmail.com>
2017-05-23 18:13:39 +0800

    配置spring-webmvc项目

---
+++ src/main/java/com/wildcat/demo/MainController.java
@@ -0,0 +1,17 @@
+package com.wildcat.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by lixingle on 2017/5/23.
+ */
+@Controller
+public class MainController {
+    @RequestMapping(value = "/test.do",method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lixingle
+  Date: 2017/5/23
+  Time: 下午6:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
+<head>
+    <title>Title</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+测试
 </body>
 </html>
